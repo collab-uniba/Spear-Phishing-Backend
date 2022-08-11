5742e16fc81aca215a151c3f7d467f427d7bfd58
Multipask <pasquale.monniello00@gmail.com>
2022-08-11 22:43:55 +0200

    Parsing updated. Automatic upload from CSV complete.

--- src/test/java/PasqualeMonniello/SpearPhishingBackend/Builders/ParseFiles.java
+++ src/test/java/PasqualeMonniello/SpearPhishingBackend/Builders/ParseFiles.java
@@ -2,32 +2,72 @@ package PasqualeMonniello.SpearPhishingBackend.Builders;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.converter.EmailConverter;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import PasqualeMonniello.SpearPhishingBackend.model.User;
+
 public class ParseFiles {
 	
+	public static String PATH = "E:\\\\CodeProjects\\\\Eclipse\\\\SpearPhishingBackend\\\\src\\\\emails\\";
+	
 	public static void main(String[] args) throws FileNotFoundException {
-		File folder = new File("E:\\\\CodeProjects\\\\Eclipse\\\\SpearPhishingBackend\\\\src\\\\emails");
-		File[] listOfFiles = folder.listFiles();
 		
-		for(File f : listOfFiles) {
+	}
+	
+	public static File[] getFilesByEmail(String email){
+		File folder = new File(PATH+email);
+		File[] list = folder.listFiles();
+
+		for(File f:list) {
 			if(f.isFile()) {
-				Email email = EmailConverter.emlToEmail(f);
-				uploadEmail(email);
+				Email e = EmailConverter.emlToEmail(f);
+				
+				uploadEmail(e, email, isEmailPhishing(f));
 			}
+		}
+		
+		return list;
+	}
+	
+	public static void parseCSV() {
+		try {
+			File csv = new File(PATH+"users.csv");
+			FileReader filereader = new FileReader(csv);
+			CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(1)
+                    .build();
+			List<String[]> allData = csvReader.readAll();
+			  
+	        // print Data
+	        for (String[] row : allData) {
+	            uploadUser(new User(row[0],row[1],row[2],row[3],row[4]));
+	        }
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
+	public static boolean isEmailPhishing(File f) {
+		return f.getName().toUpperCase().matches("_P$");
+	}
 	
-	public static void uploadEmail(Email em) {
+	public static void uploadUser(User u) {
 		try {
 			//registra il driver
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -39,7 +79,43 @@ public class ParseFiles {
 			//creaimo l'ambiente per eseguire le query
 			
 			
-			String query  = "insert into emails (FK_target, content) values(?,?)";
+			String query  = "insert into users (email, fname, lname, position, phonenumber) values(?,?,?,?,?)";
+					
+			
+			PreparedStatement preparestmt = con.prepareStatement(query);
+			
+			preparestmt.setString(1, u.getEmail());
+			preparestmt.setString(2,u.getFname());
+			preparestmt.setString(3,u.getLname());
+			preparestmt.setString(4,u.getPosition());
+			preparestmt.setString(5,u.getPhonenumber());
+			
+			preparestmt.executeUpdate();
+			
+			preparestmt.close();
+			con.close();
+
+			//eseguiamo la query
+		
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void uploadEmail(Email em, String target, Boolean phish) {
+		try {
+			//registra il driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//esegue la connessione
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/spearphishing","root","admin");
+			System.out.println("Connessione effettuata");
+			
+			//creaimo l'ambiente per eseguire le query
+			
+			
+			String query  = "insert into emails (FK_target, content, isPhish) values(?,?,?)";
 			
 			JSONObject json = JSONBuilder.buildEmail(em);
 			
@@ -47,8 +123,9 @@ public class ParseFiles {
 			
 			PreparedStatement preparestmt = con.prepareStatement(query);
 			
-			preparestmt.setString(1, em.getRecipients().get(0).getAddress());
+			preparestmt.setString(1, target);
 			preparestmt.setString(2,json.toString());
+			preparestmt.setBoolean(3, phish);
 			
 			preparestmt.executeUpdate();
 			
